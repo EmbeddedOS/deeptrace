552889e3fdb4ced1f215a3697c02e0840427150a
EmbeddedOS <nguyenthanhcong12gtm1@gmail.com>
2025-08-18 23:31:46 +0700

    Add tool options.

--- tools/sbench.cc
+++ tools/sbench.cc
@@ -1,12 +1,49 @@
+#include <unistd.h>
 #include <iostream>
 
-int main() {
-    // Initialize the syscall benchmark tool
-    // This is where the syscall benchmarking logic will be implemented
-    // For now, we can just print a message to indicate that the tool has started
-    std::cout << "Syscall benchmark tool started successfully!" << std::endl;
+int main(int argc, char *argv[])
+{
+    std::string pid{};
+    std::string output_file{};
 
-    // Main loop or functionality of the syscall benchmark tool would go here
+    auto usage = [](int exit_code)
+    {
+        std::cerr << "Usage: sbench [-p pid] [-o output_file] [-h]\n"
+                  << "Options:\n"
+                  << "  -p pid          Specify the process ID to benchmark\n"
+                  << "  -o output_file  Specify the output file for results\n"
+                  << "  -h              Display this help message\n";
+        exit(exit_code);
+    };
+
+    if (argc < 1)
+    {
+        usage(EXIT_FAILURE);
+    }
+
+    int opt = 0;
+    while ((opt = getopt(argc, argv, "hp:o:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+            pid = std::string{optarg};
+            break;
+        case 'o':
+            output_file = std::string{optarg};
+            break;
+        case 'h':
+            usage(EXIT_SUCCESS);
+            break;
+        default:
+            usage(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = optind; i < argc; ++i)
+    {
+        std::cerr << "Process name " << argv[i] << "\n";
+    }
 
     return 0;
 }
